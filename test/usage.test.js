7e1254faeb00556a57f71113c71035c32f1381c0
Lee Presswood <LeePresswood@gmail.com>
2018-11-05 17:07:10 -0600

    Tested and documented getStateOf().

---
+++ test/usage.test.js
@@ -0,0 +1,68 @@
+const {
+    Block
+} = require('../src/index');
+
+describe("usage", () => {
+    test("can make individual pushes to the blockchain", () => {
+        let chain = new Block({
+            abc: 123
+        });
+        chain.push({
+            aaa: 321
+        })
+        chain.push({
+            xyz: 999
+        });
+
+        expect(chain.getStateOf("abc")).toBe(123);
+        expect(chain.getStateOf("aaa")).toBe(321);
+        expect(chain.getStateOf("xyz")).toBe(999);
+    });
+
+    test("can replace chain with individual pushes to the blockchain", () => {
+        let chain = new Block({
+            abc: 123
+        });
+
+        chain = chain.push({
+            aaa: 321
+        })
+        chain.push({
+            xyz: 999
+        });
+
+        expect(chain.getStateOf("abc")).toBe(123);
+        expect(chain.getStateOf("aaa")).toBe(321);
+        expect(chain.getStateOf("xyz")).toBe(999);
+    });
+
+    test("can chain pushes to the blockchain", () => {
+        let chain = new Block({
+            abc: 123
+        }).push({
+            aaa: 321
+        }).push({
+            xyz: 999
+        });
+
+        expect(chain.getStateOf("abc")).toBe(123);
+        expect(chain.getStateOf("aaa")).toBe(321);
+        expect(chain.getStateOf("xyz")).toBe(999);
+    });
+
+    test("can chain pushes to the blockchain after original creation", () => {
+        let chain = new Block({
+            abc: 123
+        });
+
+        chain.push({
+            aaa: 321
+        }).push({
+            xyz: 999
+        });
+
+        expect(chain.getStateOf("abc")).toBe(123);
+        expect(chain.getStateOf("aaa")).toBe(321);
+        expect(chain.getStateOf("xyz")).toBe(999);
+    });
+});
